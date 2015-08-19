ff4e8628574a7907087199064d47ea357247b39c
Kegan Dougal <kegan@matrix.org>
2015-08-19 17:06:20 +0100

    Add stub user bridge store tests

---
+++ spec/integ/user-bridge-store.spec.js
@@ -0,0 +1,73 @@
+"use strict";
+
+describe("UserBridgeStore", function() {
+    beforeEach(function(done) {
+        done();
+    });
+
+    describe("storeMatrixUser", function() {
+
+        it("should be able to store a Matrix user, retrievable again via getByMatrixId",
+        function() {
+
+        });
+    });
+
+    describe("storeJungleUser", function() {
+
+        it("should be able to store a Jungle user, retrievable again via getByJungleId",
+        function() {
+
+        });
+
+        it("should fully persist all types of primitive data", function() {
+
+        });
+
+        it("should not persist functions", function() {
+
+        });
+    });
+
+    describe("getByJungleData", function() {
+        it("should be able to retrieve via top level keys", function() {
+
+        });
+
+        it("should be able to retrieve via nested keys", function() {
+
+        });
+
+        it("should be able to use basic NoSQL $commands", function() {
+
+        });
+    });
+
+    describe("getMatrixUsersFromJungleId", function() {
+        it("should return an empty array if there are no matches", function() {
+
+        });
+
+        it("should return a list of users for multiple matches", function() {
+
+        });
+
+        it("should return a single element list for a single match", function() {
+
+        });
+    });
+
+    describe("getJungleUsersFromMatrixId", function() {
+        it("should return an empty array if there are no matches", function() {
+
+        });
+
+        it("should return a list of users for multiple matches", function() {
+
+        });
+
+        it("should return a single element list for a single match", function() {
+
+        });
+    });
+});

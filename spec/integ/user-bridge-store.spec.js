1f169add6e2f032f3d371c3365fea82727933f07
Kegan Dougal <kegan@matrix.org>
2015-08-20 11:35:04 +0100

    Add basic get/set tests

--- spec/integ/user-bridge-store.spec.js
+++ spec/integ/user-bridge-store.spec.js
@@ -1,36 +1,107 @@
 "use strict";
+var Datastore = require("nedb");
+var fs = require("fs");
+var log = require("../log");
 
 var UserBridgeStore = require("../..").UserBridgeStore;
+var MatrixUser = require("../..").MatrixUser;
+var JungleUser = require("../..").JungleUser;
+var TEST_DB_PATH = __dirname + "/test.db";
 
 describe("UserBridgeStore", function() {
-    var store;
+    var store, db;
 
     beforeEach(function(done) {
-        store = new UserBridgeStore();
-        done();
+        log.beforeEach(this);
+        db = new Datastore({
+            filename: TEST_DB_PATH,
+            autoload: true,
+            onload: function(err) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                store = new UserBridgeStore(db);
+                done();
+            }
+        });
+    });
+
+    afterEach(function() {
+        fs.unlinkSync(TEST_DB_PATH);
     });
 
     describe("storeMatrixUser", function() {
-
         it("should be able to store a Matrix user, retrievable again via getByMatrixId",
-        function() {
-
+        function(done) {
+            var userId = "@foo:bar";
+            var user = new MatrixUser(userId);
+            user.setDisplayName("Foo");
+            store.storeMatrixUser(user).then(function() {
+                return store.getByMatrixId(userId);
+            }).done(function(userFromStore) {
+                expect(userFromStore.getId()).toEqual(userId);
+                expect(userFromStore.getDisplayName()).toEqual("Foo");
+                done();
+            });
         });
     });
 
     describe("storeJungleUser", function() {
-
         it("should be able to store a Jungle user, retrievable again via getByJungleId",
-        function() {
-
+        function(done) {
+            var jungleId = "some_unique_id";
+            var user = new JungleUser(jungleId);
+            store.storeJungleUser(user).then(function() {
+                return store.getByJungleId(jungleId);
+            }).done(function(userFromStore) {
+                expect(userFromStore.getId()).toEqual(jungleId);
+                done();
+            });
         });
 
-        it("should fully persist all types of primitive data", function() {
-
+        it("should fully persist all types of primitive data", function(done) {
+            var jungleId = "some_unique_id";
+            var user = new JungleUser(jungleId);
+            user.set("int", 42);
+            user.set("str", "the answer");
+            user.set("bool", true);
+            user.set("obj", {
+                foo: "bar",
+                baz: {
+                    buzz: true
+                }
+            });
+            store.storeJungleUser(user).then(function() {
+                return store.getByJungleId(jungleId);
+            }).done(function(userFromStore) {
+                expect(userFromStore.getId()).toEqual(jungleId);
+                expect(userFromStore.get("int")).toEqual(42);
+                expect(userFromStore.get("str")).toEqual("the answer");
+                expect(userFromStore.get("bool")).toEqual(true);
+                expect(userFromStore.get("obj")).toEqual({
+                    foo: "bar",
+                    baz: {
+                        buzz: true
+                    }
+                });
+                done();
+            });
         });
 
-        it("should not persist functions", function() {
-
+        it("should not persist functions", function(done) {
+            var jungleId = "some_unique_id";
+            var user = new JungleUser(jungleId);
+            user.set("fn", function(foo) {
+                return 42;
+            });
+            store.storeJungleUser(user).then(function() {
+                return store.getByJungleId(jungleId);
+            }).done(function(userFromStore) {
+                expect(userFromStore.getId()).toEqual(jungleId);
+                expect(userFromStore.get("fn")).toBeUndefined();
+                done();
+            });
         });
     });
 
---
+++ spec/log.js
@@ -0,0 +1,5 @@
+module.exports.beforeEach = function(testCase) {
+    var desc = testCase.suite.description + " : " + testCase.description;
+    console.log(desc);
+    console.log(new Array(1 + desc.length).join("="));
+};

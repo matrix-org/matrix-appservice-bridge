4bf557cbbea4c08d1b9aece61f968cf36eb60532
Kegan Dougal <kegan@matrix.org>
2015-08-27 15:58:13 +0100

    Hook up --generate-registration to the Cli and example

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 "use strict";
+var AppServiceRegistration = require("matrix-appservice").AppServiceRegistration;
 var bridgeLib = require("./");
 var Cli = bridgeLib.Cli;
 var Bridge = bridgeLib.Bridge;
@@ -11,9 +12,25 @@ var bridgeInst = new Bridge({
 });
 
 var c = new Cli({
-    bridge: bridgeInst, // the thing which will be called with run(port,config)
-    registrationPath: "my-bridge-registration.yaml", // where gen reg will dump to
-    port: 8155 // default port, CLI can override with --port
+    // Required. the thing which will be called with run(port,config)
+    bridge: bridgeInst,
+
+    // Optional: where --generate-registration will dump to
+    registrationPath: "my-bridge-registration.yaml",
+
+    // Optional: default port, CLI can override with --port
+    port: 8155,
+
+    // Required if enableRegistration is true.
+    // make the registration file. Callback provided so you can do DB hits.
+    generateRegistration: function(callback) {
+        var reg = new AppServiceRegistration("http://localhost:8008");
+        reg.setHomeserverToken(AppServiceRegistration.generateToken());
+        reg.setAppServiceToken(AppServiceRegistration.generateToken());
+        reg.setSenderLocalpart("bridge-example");
+        reg.addRegexPattern("users", "@example_.*", true);
+        callback(reg);
+    }
 });
 
 c.run(); // check system args

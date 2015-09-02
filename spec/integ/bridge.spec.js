8f6dd27b8f753b460c06e17c477cad1ad0907757
Kegan Dougal <kegan@matrix.org>
2015-09-02 16:09:32 +0100

    Implement skeleton structs for running a Bridge instance

--- spec/integ/bridge.spec.js
+++ spec/integ/bridge.spec.js
@@ -1,9 +1,108 @@
 "use strict";
+var Promise = require("bluebird");
+var Datastore = require("nedb");
+var fs = require("fs");
+var log = require("../log");
+
+var HS_URL = "http://example.com";
+var HS_DOMAIN = "example.com";
+var BOT_LOCALPART = "the_bridge";
+
+var TEST_USER_DB_PATH = __dirname + "/test-users.db";
+var TEST_ROOM_DB_PATH = __dirname + "/test-rooms.db";
+var UserBridgeStore = require("../..").UserBridgeStore;
+var RoomBridgeStore = require("../..").RoomBridgeStore;
+var Bridge = require("../..").Bridge;
+
 describe("Bridge", function() {
-    // var bridge, appService;
+    var bridge, bridgeCtrl, appService, clientFactory, appServiceRegistration;
 
-    beforeEach(function() {
+    beforeEach(
+    /** @this */
+    function(done) {
+        log.beforeEach(this);
+        clientFactory = jasmine.createSpyObj("ClientFactory", [
+            "setLogFunction", "getClientAs", "configure"
+        ]);
+        appService = jasmine.createSpyObj("AppService", [
+            "onAliasQuery", "onUserQuery", "listen", "on"
+        ]);
+        appService._events = {};
+        appService.on.andCallFake(function(name, fn) {
+            if (!appService._events[name]) {
+                appService._events[name] = [];
+            }
+            appService._events[name].push(fn);
+        });
+        bridgeCtrl = jasmine.createSpyObj("controller", [
+            "onEvent", "onAliasQuery", "onUserQuery"
+        ]);
+        appServiceRegistration = jasmine.createSpyObj("AppServiceRegistration", [
+            "getOutput", "isUserMatch", "isAliasMatch", "isRoomMatch"
+        ]);
+        appServiceRegistration.getOutput.andReturn({
+            hs_token: "h5_t0k3n",
+            as_token: "a5_t0k3n",
+            url: "http://app-service-url",
+            sender_localpart: BOT_LOCALPART,
+            namespaces: {
+                users: [{
+                    exclusive: true,
+                    regex: "@virtual_.*"
+                }],
+                aliases: [{
+                    exclusive: true,
+                    regex: "#virtual_.*"
+                }]
+            }
+        });
 
+        function loadDatabase(path, Cls) {
+            var defer = Promise.defer();
+            var db = new Datastore({
+                filename: path,
+                autoload: true,
+                onload: function(err) {
+                    if (err) {
+                        defer.reject(err);
+                        return;
+                    }
+                    defer.resolve(new Cls(db));
+                }
+            });
+            return defer.promise;
+        }
+
+        Promise.all([
+            loadDatabase(TEST_USER_DB_PATH, UserBridgeStore),
+            loadDatabase(TEST_ROOM_DB_PATH, RoomBridgeStore)
+        ]).spread(function(userDb, roomDb) {
+            bridge = new Bridge({
+                homeserverUrl: HS_URL,
+                domain: HS_DOMAIN,
+                registration: appServiceRegistration,
+                userStore: userDb,
+                roomStore: roomDb,
+                controller: bridgeCtrl,
+                clientFactory: clientFactory
+            });
+            done();
+        });
+    });
+
+    afterEach(function() {
+        try {
+            fs.unlinkSync(TEST_USER_DB_PATH);
+        }
+        catch(e) {
+            // do nothing
+        }
+        try {
+            fs.unlinkSync(TEST_ROOM_DB_PATH);
+        }
+        catch(e) {
+            // do nothing
+        }
     });
 
     describe("onUserQuery", function() {
@@ -85,8 +184,16 @@ describe("Bridge", function() {
     });
 
     describe("run", function() {
-        it("should emit a 'run' event with (port, config)", function() {
-
+        it("should emit a 'run' event with (port, config)", function(done) {
+            var testConfig = {
+                foo: "bar"
+            };
+            bridge.on("run", function(port, config) {
+                expect(port).toEqual(101);
+                expect(config).toEqual(testConfig);
+                done();
+            })
+            bridge.run(101, testConfig, appService);
         });
 
         it("should invoke listen(port) on the AppService instance", function() {

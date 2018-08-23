4391034bcc80c660105f5d9a7235032aef6aaaf5
Will Hunt <will@half-shot.uk>
2018-08-23 13:17:09 +0100

    Cache now limits it's size

--- spec/unit/client-request-cache.spec.js
+++ spec/unit/client-request-cache.spec.js
@@ -4,8 +4,9 @@ const Promise = require("bluebird");
 describe("ClientRequestCache", function() {
     describe("constructor", function() {
         it("Can construct", function() {
-            const crc = new ClientRequestCache(50, () => { });
+            const crc = new ClientRequestCache(50, 1, () => { });
             expect(crc.ttl).toBe(50);
+            expect(crc.maxSize).toBe(1);
             expect(crc.requestFunc).toBeDefined();
             expect(crc._requestContent).toBeDefined();
         });
@@ -13,19 +14,19 @@ describe("ClientRequestCache", function() {
         /* eslint-disable no-new */
         it("Cannot construct with incorrect parameters", function() {
             expect(() => { new ClientRequestCache() }).toThrow();
-            expect(() => { new ClientRequestCache(0, () => { })}).toThrow();
-            expect(() => { new ClientRequestCache(50, undefined)}).toThrow();
-            expect(() => { new ClientRequestCache("apple", () => { })}).toThrow();
+            expect(() => { new ClientRequestCache(0, 0, () => { })}).toThrow();
+            expect(() => { new ClientRequestCache(50, 1, undefined)}).toThrow();
+            expect(() => { new ClientRequestCache("apple", "banana", () => { })}).toThrow();
             expect(() => { new ClientRequestCache(50, "apple")}).toThrow();
-            expect(() => { new ClientRequestCache(-1, () => { })}).toThrow();
-            expect(() => { new ClientRequestCache(0, () => { })}).toThrow();
-            expect(() => { new ClientRequestCache(50.5, () => { })}).toThrow();
+            expect(() => { new ClientRequestCache(-1, -2, () => { })}).toThrow();
+            expect(() => { new ClientRequestCache(0, 0, () => { })}).toThrow();
+            expect(() => { new ClientRequestCache(50.5, 45.1, () => { })}).toThrow();
         });
         /* eslint-enable no-new */
     });
     describe("get", function() {
         it("should fetch a non-cached item", () => {
-            const crc = new ClientRequestCache(50000, () => {
+            const crc = new ClientRequestCache(50000, 1, () => {
                 return "Behold, the *thing*";
             });
             return crc.get("athing").then((res) => {
@@ -34,7 +35,7 @@ describe("ClientRequestCache", function() {
         });
         it("should store in the cache", () => {
             let requestCount = 0;
-            const crc = new ClientRequestCache(50000, () => {
+            const crc = new ClientRequestCache(50000, 1, () => {
                 requestCount++;
                 return "Behold, the *thing*";
             });
@@ -47,7 +48,7 @@ describe("ClientRequestCache", function() {
         });
         it("should expire old items", () => {
             let requestCount = 0;
-            const crc = new ClientRequestCache(50, () => {
+            const crc = new ClientRequestCache(50, 2, () => {
                 requestCount++;
                 return "Behold, the *thing*";
             });
@@ -61,7 +62,7 @@ describe("ClientRequestCache", function() {
             });
         });
         it("should hold multiple items", () => {
-            const crc = new ClientRequestCache(1000, (thing) => {
+            const crc = new ClientRequestCache(1000, 2, (thing) => {
                 if (thing === "1") {
                     return "Thing 1!";
                 }
@@ -76,7 +77,7 @@ describe("ClientRequestCache", function() {
             });
         });
         it("should pass down failures", () => {
-            const crc = new ClientRequestCache(1000, () => {
+            const crc = new ClientRequestCache(1000, 1, () => {
                 return Promise.reject("Sorry, this test has subject to a GDPR request.");
             });
             return crc.get("1").then((res) => {
@@ -86,7 +87,7 @@ describe("ClientRequestCache", function() {
             });
         });
         it("should pass args", () => {
-            const crc = new ClientRequestCache(1000, (key, ...args) => {
+            const crc = new ClientRequestCache(1000, 1, (key, ...args) => {
                 return args;
             });
             return crc.get("1", "Hey", "that's", "pretty", "cool").then((res) => {
@@ -94,12 +95,26 @@ describe("ClientRequestCache", function() {
             });
         });
         it("should reject non-string keys", () => {
-            const crc = new ClientRequestCache(1000, () => { });
+            const crc = new ClientRequestCache(1000, 1, () => { });
             expect(() => { crc.get({}) }).toThrow();
             expect(() => { crc.get(1) }).toThrow();
             expect(() => { crc.get(true) }).toThrow();
             expect(() => { crc.get(null) }).toThrow();
             expect(() => { crc.get() }).toThrow();
+        });
+        it("should respect max size", () => {
+            const crc = new ClientRequestCache(1000, 2, (key) => `${key}baa`);
+            return Promise.all([
+                crc.get("1"),
+                crc.get("2"),
+                crc.get("3"),
+                crc.get("4"),
+            ]).then(() => {
+                expect([...crc._requestContent.values()].map((v) => v.content)).toEqual([
+                    "3baa",
+                    "4baa",
+                ]);
+            });
         });
     });
 });

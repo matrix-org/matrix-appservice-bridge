3443225d25c5a34831a0492da39f855646a69fd0
Will Hunt <will@half-shot.uk>
2018-10-02 18:54:26 +0100

    Indentation

--- spec/unit/matrix-user.spec.js
+++ spec/unit/matrix-user.spec.js
@@ -2,18 +2,19 @@ const MatrixUser = require("../../lib/models/users/matrix");
 
 describe("MatrixUser", function() {
     describe("escapeUserId", function() {
-        it("should not escape acceptable userids", function() {
-            [new MatrixUser("@test:localhost", null, false),
-            new MatrixUser("@42:localhost", null, false),
-            new MatrixUser("@Test42:localhost", null, false),
-            new MatrixUser("@A=Good-set.of_chars:localhost", null, false)
+        it("should not escape acceptable user ids", function() {
+            [
+                new MatrixUser("@test:localhost", null, false),
+                new MatrixUser("@42:localhost", null, false),
+                new MatrixUser("@Test42:localhost", null, false),
+                new MatrixUser("@A=Good-set.of_chars:localhost", null, false)
             ].forEach((user) => {
                 const userId = user.getId();
                 user.escapeUserId();
                 expect(user.getId()).toBe(userId);
             })
         });
-        it("should escape unacceptable userids", function() {
+        it("should escape unacceptable user ids", function() {
             const expected = [
                 "@=24:localhost",
                 "@500=24=20dog:localhost",
@@ -21,11 +22,12 @@ describe("MatrixUser", function() {
                 "@=D83D:localhost",
                 "@matrix.org=2Fspec:localhost",
             ];
-            [new MatrixUser("@$:localhost", null, false),
-            new MatrixUser("@500$ dog:localhost", null, false),
-            new MatrixUser("@woah***:localhost", null, false),
-            new MatrixUser("@🐶:localhost", null, false),
-            new MatrixUser("@matrix.org/spec:localhost", null, false)
+            [
+                new MatrixUser("@$:localhost", null, false),
+                new MatrixUser("@500$ dog:localhost", null, false),
+                new MatrixUser("@woah***:localhost", null, false),
+                new MatrixUser("@🐶:localhost", null, false),
+                new MatrixUser("@matrix.org/spec:localhost", null, false)
             ].forEach((user, i) => {
                 user.escapeUserId();
                 expect(user.getId()).toBe(expected[i]);

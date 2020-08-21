8a4bfcb117f0df52d0e5c382902dffb525bd16ad
Will Hunt <will@half-shot.uk>
2020-08-21 14:49:20 +0100

    Apply suggestions from code review
    Co-authored-by: Christian Paul <christianp@matrix.org>

--- src/components/config-validator.ts
+++ src/components/config-validator.ts
@@ -30,14 +30,13 @@ export default class ConfigValidator {
 
     /**
      * Validate the input config.
-     * @param {string|Object} inputConfig The input config file path (string) or
+     * @param inputConfig The input config file path (string) or
      * parsed config (Object).
-     * @param {Object=} defaultConfig The default config options.
-     * @return {Object} The input config with defaults applied.
+     * @param defaultConfig The default config options.
+     * @return The input config with defaults applied.
      * @throws On validation errors
      */
     public validate(inputConfig: string|Schema, defaultConfig: Record<string, unknown> = {}) {
-        defaultConfig = defaultConfig || {};
         if (typeof inputConfig === "string") {
             inputConfig = ConfigValidator.loadFromFile(inputConfig);
         }

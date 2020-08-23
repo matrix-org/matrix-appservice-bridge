5d7a06a719e1a64a3bbc57c623bdb546e9895c57
Will Hunt <will@half-shot.uk>
2020-08-23 14:08:03 +0100

    Port Event Bridge Store

---
+++ src/components/event-bridge-store.ts
@@ -0,0 +1,105 @@
+/*
+Copyright 2019 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import Datastore from "nedb";
+import { BridgeStore } from "./bridge-store";
+import { StoredEvent } from "../models/events/event";
+
+/**
+ * Construct a store suitable for event mapping information. Data is stored
+ * as {@link StoredEvent}s.
+ * @constructor
+ * @param {Datastore} db The connected NEDB database instance
+ */
+export class EventBridgeStore extends BridgeStore {
+    constructor(db: Datastore) { super(db) }
+
+    /**
+     * Insert an event, clobbering based on the ID of the StoredEvent.
+     * @param event
+     */
+    public upsertEvent(event: StoredEvent) {
+        return this.upsert({
+            id: event.getId()
+        }, event.serialize());
+    }
+
+    /**
+     * Get an existing event based on the provided matrix IDs.
+     * @param {string} roomId The ID of the room.
+     * @param {string} eventId The ID of the event.
+     * @return {?StoredEvent} A promise which resolves to the StoredEvent or null.
+     */
+    public getEntryByMatrixId(roomId: string, eventId: string): Promise<StoredEvent|null> {
+        return this.selectOne<any, StoredEvent>({
+            "matrix.roomId": roomId,
+            "matrix.eventId": eventId,
+        }, (this.convertTo(function(doc) {
+            return StoredEvent.deserialize(doc);
+        })) as (doc: any) => StoredEvent);
+    }
+
+    /**
+     * Get an existing event based on the provided remote IDs.
+     * @param {string} roomId The ID of the room.
+     * @param {string} eventId The ID of the event.
+     * @return {?StoredEvent} A promise which resolves to the StoredEvent or null.
+     */
+    public getEntryByRemoteId(roomId: string, eventId: string) {
+        return this.selectOne({
+            "remote.roomId": roomId,
+            "remote.eventId": eventId,
+        }, this.convertTo((doc: any) => {
+            return StoredEvent.deserialize(doc);
+        }));
+    }
+
+    /**
+     * Remove entries based on the event data.
+     * @param {StoredEvent} event The event to remove.
+     * @return {Promise}
+     */
+    public removeEvent(event: StoredEvent) {
+        return this.delete({
+            id: event.getId(),
+        });
+    }
+
+    /**
+     * Remove entries based on the matrix IDs.
+     * @param {string} roomId The ID of the room.
+     * @param {string} eventId The ID of the event.
+     * @return {Promise}
+     */
+    public removeEventByMatrixId(roomId: string, eventId: string) {
+        return this.delete({
+            "matrix.roomId": roomId,
+            "matrix.eventId": eventId,
+        });
+    }
+
+    /**
+     * Remove entries based on the matrix IDs.
+     * @param {string} roomId The ID of the room.
+     * @param {string} eventId The ID of the event.
+     * @return {Promise}
+     */
+    public removeEventByRemoteId(roomId: string, eventId: string) {
+        return this.delete({
+            "remote.roomId": roomId,
+            "remote.eventId": eventId,
+        });
+    }
+}

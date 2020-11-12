f24773c81ad9325134fb9d03f63f2e955af49a82
Will Hunt <willh@matrix.org>
2020-11-12 11:12:17 +0000

    newline

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -2,7 +2,7 @@
 # This script will generate documentation for the current version
 
 
-VERSION=`python3 -c "import json; f = open('./package.json', 'r'); v = json.loads(f.read())['version']; f.close(); print(v)"`
+VERSION="2.3.1a" #`python3 -c "import json; f = open('./package.json', 'r'); v = json.loads(f.read())['version']; f.close(); print(v)"`
 
 rm -R .typedoc
 
@@ -12,12 +12,12 @@ set -e
 yarn gendoc
 
 git checkout gh-pages
-mv .typedoc $VERSION
-sed "6i<li><a href=\"$VERSION/index.html\">$VERSION</a></li>" index.html > index.html.new
+mv .typedoc/ $VERSION/
+sed "6i\n<li><a href=\"$VERSION/index.html\">$VERSION</a></li>" index.html > index.html.new
 rm index.html
 mv index.html.new index.html
 
 git add "$VERSION/"
 git add index.html
-git commit -m "Add documentation for $VERSION"
+# git commit -m "Add documentation for $VERSION"
 # git push

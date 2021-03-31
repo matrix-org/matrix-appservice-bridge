95bfe08ce8be2cdb78a31afd6590028da890ffb2
Eric Eastwood <contact@ericeastwood.com>
2021-03-30 22:01:31 -0500

    Clean up hook and make sure build in right place

--- hooks/postinstall.js
+++ hooks/postinstall.js
@@ -2,15 +2,25 @@ const path = require('path');
 const fs = require('fs');
 // eslint-disable-next-line camelcase
 const child_process = require('child_process');
+const package = require('../package.json');
+
+const PROJECT_ROOT = path.join(__dirname, '../');
 
 async function doPostInstall() {
+  const mainEntry = path.join(PROJECT_ROOT, package.main);
   try {
-    fs.statSync(path.join(__dirname, '../lib/index.js'));
+    fs.statSync(mainEntry);
   }
   catch (err) {
-    console.log('Building typescript because the package.json main entry points to a non-existent location');
-    child_process.exec('npm install typescript');
-    child_process.exec('npm run prepare');
+    // eslint-disable-next-line max-len
+    console.log(`matrix-appservice-bridge: Building TypeScript because the package.json main entry (${mainEntry}) points to a non-existent location.`);
+
+    child_process.exec('npm install typescript', {
+      cwd: PROJECT_ROOT
+    });
+    child_process.exec('npm run prepare', {
+      cwd: PROJECT_ROOT
+    });
   }
 }
 

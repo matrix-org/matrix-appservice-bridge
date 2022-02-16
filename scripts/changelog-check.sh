3157f99467db47c05c45cca44362e943f013da81
Half-Shot <will@half-shot.uk>
2022-02-16 10:47:16 +0000

    Add changelog check

--- scripts/changelog-check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script will install towncrier and check if a newsfile exists on the current checkout.
-
-pip3 install towncrier==19.2.0
-python3 -m towncrier.check --compare-with=origin/develop

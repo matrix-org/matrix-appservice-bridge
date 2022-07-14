a13f93bcb9c14069c609470cc373a29022e8602d
gnuxie <gnuxie@element.io>
2022-07-14 16:57:06 +0100

    ok this is better

--- src/models/users/matrix.ts
+++ src/models/users/matrix.ts
@@ -33,9 +33,9 @@ export class MatrixUser {
         if (_data && typeof _data !== "object") {
             throw Error("data arg must be an Object");
         }
-        const split = this.userId.indexOf(':');
-        this._localpart = this.userId.substring(1, split);
-        this.host = this.userId.substring(split+1);
+        const split = this.userId.split(/:(.*)/); // only split on the first colon.
+        this._localpart = split[0].substring(1);
+        this.host = split[1];
         if (escape) {
             this.escapeUserId();
         }
